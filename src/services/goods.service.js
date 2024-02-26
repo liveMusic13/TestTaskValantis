ca7992521ab5e6882eebb08cddb8ad16688bf006
Dmitriy <aegis.ko20@mail.ru>
2024-02-26 12:43:46 +0300

    add log

--- src/services/goods.service.js
+++ src/services/goods.service.js
@@ -5,26 +5,6 @@ const uniqueIds = data => {
 };
 
 export const goodsService = {
-	// getIdItems: async (setIsLoading, setErrorState, getItems, setItems) => {
-	// 	setIsLoading(true);
-	// 	try {
-	// 		const response = await $axios.post('', {
-	// 			action: 'get_ids',
-	// 			params: { offset: 0 },
-	// 		});
-
-	// 		await getItems(
-	// 			setIsLoading,
-	// 			setItems,
-	// 			setErrorState,
-	// 			uniqueIds(response.data.result)
-	// 		);
-	// 	} catch (error) {
-	// 		setErrorState({ isError: true, status: error.response.status });
-	// 	} finally {
-	// 		setIsLoading(false);
-	// 	}
-	// },
 	getIdItems: async (setIsLoading, setErrorState, getItems, setItems) => {
 		setIsLoading(true);
 		let retry = 0;
@@ -58,55 +38,6 @@ export const goodsService = {
 			}
 		}
 	},
-	// getFilters: async (
-	// 	setIsLoading,
-	// 	setItems,
-	// 	setErrorState,
-	// 	getItems,
-	// 	dataFields
-	// ) => {
-	// 	setIsLoading(true);
-	// 	try {
-	// 		const params = {};
-	// 		if (dataFields.product) params.product = dataFields.product;
-	// 		if (dataFields.price) params.price = Number(dataFields.price);
-	// 		if (dataFields.brand) params.brand = dataFields.brand;
-
-	// 		const response = await $axios.post('', {
-	// 			action: 'filter',
-	// 			params: params,
-	// 		});
-	// 		console.log(params);
-	// 		await getItems(
-	// 			setIsLoading,
-	// 			setItems,
-	// 			setErrorState,
-	// 			uniqueIds(response.data.result)
-	// 		);
-
-	// 		console.log(response);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	} finally {
-	// 		setIsLoading(false);
-	// 	}
-	// },
-	// getItems: async (setIsLoading, setItems, setErrorState, data) => {
-	// 	setIsLoading(true);
-
-	// 	try {
-	// 		const response = await $axios.post('', {
-	// 			action: 'get_items',
-	// 			params: { ids: data },
-	// 		});
-
-	// 		setItems(response.data);
-	// 	} catch (error) {
-	// 		setErrorState({ isError: true, status: error.response.status });
-	// 	} finally {
-	// 		setIsLoading(false);
-	// 	}
-	// },
 	getFilters: async (
 		setIsLoading,
 		setItems,
@@ -138,6 +69,7 @@ export const goodsService = {
 				console.log(response);
 				break;
 			} catch (error) {
+				console.log('ошибка номер: ', error.response.status);
 				if (error.response.status === 500) {
 					retry++;
 					if (retry === 3) {
@@ -165,6 +97,7 @@ export const goodsService = {
 				setItems(response.data);
 				break;
 			} catch (error) {
+				console.log('ошибка номер: ', error.response.status);
 				if (error.response.status === 500) {
 					retry++;
 					if (retry === 3) {
